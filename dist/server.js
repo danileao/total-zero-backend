74494de165ca4c6802b98e9dcb9492975ea44f10
danileao <dani.leao89@gmail.com>
2020-05-05 13:17:21 -0300

    Add babel

--- dist/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _app = _interopRequireDefault(require("./app"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-_app.default.listen(3333, () => {
-  console.log("Serviço rodando na porta 3333!");
-});
