219fe362d491b9ebf26908a86b5747acbe7e7572
Daniele <dani.leao89@gmail.com>
2020-05-05 10:08:29 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,17 @@ import SessionController from "./controllers/SessionController";
 import auth from "./middlewares/auth";
 
 const routes = new Router();
+app.get("/teste", (request, response) => {
+  const projetos = [
+    { id: 1, name: "Projeto Nodejs" },
+    { id: 2, name: "Projeto ReactJS" },
+    { id: 3, name: "Projeto Deploy" },
+    { id: 4, name: "Projeto Deploy em processo" },
+    { id: 5, name: "Projeto Deploy Finalizado" },
+  ];
 
+  return response.json(projetos);
+});
 routes.post("/users", UserController.create);
 routes.post("/session", SessionController.create);
 
